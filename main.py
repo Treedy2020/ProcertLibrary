b6689124757d87fa842d26338bd05a90072a692b
TreeDy <75835140+Treedy2020@users.noreply.github.com>
2025-05-06 01:43:15 +0000

    Added the main processing script.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from utils import query_standards_by_playwright_browser
 
+
 if __name__ == "__main__":
-    query_standards_by_playwright_browser("EN 60335-1")
+    import json
+    import jsonlines
+    with open("./data/standard_code.json", "r", encoding="utf-8") as f:
+        standard_codes = json.load(f)
+
+    with jsonlines.open("data/query_result.jsonl", mode="w") as writer:
+        process = query_standards_by_playwright_browser(*standard_codes) 
+        for res in process:
+            writer.write(res)
